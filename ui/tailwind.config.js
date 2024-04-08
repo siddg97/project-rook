72ca86e8e83897e19d0f6273debb929bd84823ff
Prajwal Prasan <prajwal.prasan@gmail.com>
2024-04-07 23:07:32 -0400

    Setup NextUI

---
+++ ui/tailwind.config.js
@@ -0,0 +1,16 @@
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
