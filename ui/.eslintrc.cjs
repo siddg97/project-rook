f306171d7f5b0d26fc5f9f8f61aac6531f3faa96
siddg97 <g.sidd97@gmail.com>
2024-04-13 21:17:05 -0700

    Setup eslint and prettier for react code

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,18 +1,43 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'spaced-comment': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+  },
+};
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { NextUIProvider } from '@nextui-org/react'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { NextUIProvider } from '@nextui-org/react';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="dark text-foreground bg-background">
+      <main className='dark text-foreground bg-background'>
         <App />
       </main>
     </NextUIProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
