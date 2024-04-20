ecd41fa4a5434e5ebbb9acc7f7e8971c6e68c0da
siddg97 <g.sidd97@gmail.com>
2024-04-20 16:09:41 -0700

    Cleanup unused component

--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Conversation } from './Conversation.tsx';
 export { default as MessageCard } from './MessageCard.tsx';
-export { default as TeamAvatar } from './TeamAvatar.tsx';
 export { default as PromptInput } from './PromptInput.tsx';
 export { default as PromptInputWithActions } from './PromptInputWithActions.tsx';
 export { default as PageContainer } from './PageContainer.tsx';
